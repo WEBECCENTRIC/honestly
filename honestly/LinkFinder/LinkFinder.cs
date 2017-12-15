fa51a0c3ed7317749aafafb0fe71a0382e30ab5c
Johnny Harbieh <johnny@webeccentric.com>
2017-12-14 21:28:16 -0600

    Added exception handling

--- honestly/LinkFinder/LinkFinder.cs
+++ honestly/LinkFinder/LinkFinder.cs
@@ -8,51 +8,61 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections;
 using HtmlAgilityPack;
+using System;
 
 namespace honestly
 {
     public static class LinkFinder
     {
         /// <summary>
+        /// Use this function to retrieve all anchor tags HREF attribute values from a webpage
+        /// Install HTTPie: https://httpie.org/
         /// Use HTTPie to make a POST request: https://httpie.org/doc#http-method
         /// C:\>http POST http://localhost:7071/api/LinkFinder url="http://www.webeccentric.com"
         /// </summary>
         /// <param name="req"></param>
         /// <param name="log"></param>
-        /// <returns></returns>
+        /// <returns>A collection of URLs in JSON format</returns>
         [FunctionName("LinkFinder")]
         public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "post")]HttpRequestMessage req, TraceWriter log)
         {
-            // Get request body
-            dynamic data = req.Content.ReadAsAsync<object>().Result;
-
-            // Set url to query string or body data
-            string url_value = data?.url;
-
-            string content = null;
-            ArrayList list = new ArrayList();
-
-            using (WebClient client = new WebClient())
+            try
             {
-                byte[] page_data = client.DownloadData(new System.Uri(url_value));
+                // Get request body
+                dynamic data = req.Content.ReadAsAsync<object>().Result;
 
-                UTF8Encoding utf8 = new UTF8Encoding();
+                // Set url to query string or body data
+                string url_value = data?.url;
 
-                content = utf8.GetString(page_data);                
+                string content = null;
+                ArrayList list = new ArrayList();
+
+                using (WebClient client = new WebClient())
+                {
+                    byte[] page_data = client.DownloadData(new System.Uri(url_value));
+
+                    UTF8Encoding utf8 = new UTF8Encoding();
+
+                    content = utf8.GetString(page_data);
+                }
+
+                var document = new HtmlDocument();
+                document.LoadHtml(content);
+
+                var nodes = document.DocumentNode.Descendants("a");
+
+                foreach (HtmlNode node in nodes)
+                {
+                    if(node.Attributes["href"] != null && !String.IsNullOrEmpty(node.Attributes["href"].Value))
+                        list.Add(node.Attributes["href"].Value);
+                }
+
+                return req.CreateResponse(HttpStatusCode.OK, list);
             }
-
-            var document = new HtmlDocument();
-            document.LoadHtml(content);
-
-            var nodes = document.DocumentNode.Descendants("a");
-
-            foreach (HtmlNode node in nodes)
+            catch(Exception ex)
             {
-                list.Add(node.Attributes["href"].Value);
-            }
-
-            // Fetching the name from the path parameter in the request URL
-            return req.CreateResponse(HttpStatusCode.OK, list);
+                return req.CreateResponse(HttpStatusCode.OK, String.Format("Something went wrong while I was doing some work for you!. Error message: {0}", ex.Message));
+            }           
         }
     }
 }
